f46993e0d89cf69621a9808be31b96e3007cfa2a
limz <limzgiser@163.com>
2021-03-15 15:09:56 +0800

    元素渲染

--- react/react-start/src/App.js
+++ react/react-start/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import Button from "./components/Buttom";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button class="Button_error_ax7yz">Error Button</Button>
+        <form>
+          <input type="input" value={process.env.REACT_APP_NOT_SECRET_CODE} />   
+          当前环境：<input type="input" value={process.env.NODE_ENV } />
+        </form>
       </header>
     </div>
   );
---
+++ react/react-start/src/components/Buttom.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+// import styles from "./Buttom.module"; // Import css modules stylesheet as styles
+
+class Button extends Component {
+  render() {
+    return <button>Error Button</button>;
+  }
+}
+
+export default Button;
